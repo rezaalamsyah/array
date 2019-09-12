113dd0e330099f744d8c66a51ec650e6a70bd4d9
rezaalamsyah <53548427+rezaalamsyah@users.noreply.github.com>
2019-09-12 11:30:25 +0700

    Add files via upload

---
+++ src/array/praktikum1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author santuy
+ */
+public class praktikum1 {
+    public static void main (String[] args){
+        int bil[] = new int [5];
+        bil [0]=1;
+        bil [1]=2;
+        bil [2]=3;
+        bil [3]=4;
+        bil [4]=5;
+        System.out.println(bil[6]);
+    }
+}
---
+++ src/array/praktikum2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author santuy
+ */
+public class praktikum2 {
+    public static void main(String[] args) {
+        int bil[] = new int[5];
+        bil[0]=1;
+        bil[1]=2;
+        bil[2]=3;
+        bil[3]=4;
+        bil[4]=5;
+        bil[5]=6;
+        bil[6]=7;
+        bil[7]=8;
+    }
+}
